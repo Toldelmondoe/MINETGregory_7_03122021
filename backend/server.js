6e6e6bf704d000185a36afa2527fd455535d1994
Grégory MINET <minet.gregory@gmail.com>
2022-01-13 00:25:57 +0100

    vue init

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.get("/api", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/icon-left-redim.png"/>
+  <HelloWorld msg="Bienvenue sur le réseau social Groupomania"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
