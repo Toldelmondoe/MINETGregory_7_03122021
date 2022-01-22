c717c71b182fb41ed0521ff9a1ece2e51a827e18
Grégory MINET <minet.gregory@gmail.com>
2022-01-22 16:09:25 +0100

    vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,8 @@
 <template>
-    <div id="app">
-      <Accueil></Accueil>
-      <router-view></router-view>
-    </div>
+  <div id="nav">
+    <router-link to="/">Accueil</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import Accueil from './components/Accueil.vue'
-export default {
-    name: "App",
-    components: {
-        Accueil
-    }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,32 +1,32 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Accueil from "../components/Accueil";
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
 const routes = [
   {
-    path: "/",
-    name: "Accueil",
-    component: Accueil
-  }
-  /*{
-    path: "/signin",
-    name: "Connexion",
-    component: () => import("../components/Connexion"),
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/signin',
+    name: 'Connexion',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Connexion.vue')
   },
   {
     path: "/signup",
     name: "Inscription",
-    component: () => import("../components/Inscription")
+    component: () => import("../views/Inscription")
   },
-  {
-    path: "/account",
-    name: "Compte",
-    component: () => import("../components/Compte")
-  }*/
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
+
 /*
 router.beforeEach((to, from, next) => {
     const publicPages = ["/", "/signin", "/signup"]
